39eb79523e9089e5b69281d931dd18de7fb9883b
Thanos Tsakiris <t.than.ud@gmail.com>
2016-08-23 18:02:42 +0300

    Create SearchPattern source and header files

---
+++ source/Lib/TLibEncoder/SearchPattern.cpp
@@ -0,0 +1,11 @@
+/*
+ * SearchPattern.cpp
+ *
+ *  Created on: Aug 23, 2016
+ *      Author: Thanos Tsakiris
+ */
+
+
+
+
+
---
+++ source/Lib/TLibEncoder/SearchPattern.h
@@ -0,0 +1,15 @@
+/*
+ * SearchPattern.h
+ *
+ *  Created on: Aug 23, 2016
+ *      Author: Thanos Tsakiris
+ */
+
+#ifndef SOURCE_LIB_TLIBENCODER_SEARCHPATTERN_H_
+#define SOURCE_LIB_TLIBENCODER_SEARCHPATTERN_H_
+
+
+
+
+
+#endif /* SOURCE_LIB_TLIBENCODER_SEARCHPATTERN_H_ */
