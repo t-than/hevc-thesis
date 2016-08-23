f672597b106a8e964f549b833b1d7645d856490e
Thanos Tsakiris <t.than.ud@gmail.com>
2016-08-23 15:24:16 +0300

    Route search selection to Hexagon Search

--- source/Lib/TLibEncoder/TEncSearch.cpp
+++ source/Lib/TLibEncoder/TEncSearch.cpp
@@ -3870,7 +3870,7 @@ Void TEncSearch::xPatternSearchFast( const TComDataCU* const  pcCU,
       xTZSearch( pcCU, pcPatternKey, piRefY, iRefStride, pcMvSrchRngLT, pcMvSrchRngRB, rcMv, ruiSAD, pIntegerMv2Nx2NPred, false );
       break;
     case MESEARCH_HEXAGON_EARLY:
-      xTZSearchSelective( pcCU, pcPatternKey, piRefY, iRefStride, pcMvSrchRngLT, pcMvSrchRngRB, rcMv, ruiSAD, pIntegerMv2Nx2NPred );
+      xTZSearchHexagonEarly( pcCU, pcPatternKey, piRefY, iRefStride, pcMvSrchRngLT, pcMvSrchRngRB, rcMv, ruiSAD, pIntegerMv2Nx2NPred );
       break;
     case MESEARCH_SELECTIVE:
       xTZSearchSelective( pcCU, pcPatternKey, piRefY, iRefStride, pcMvSrchRngLT, pcMvSrchRngRB, rcMv, ruiSAD, pIntegerMv2Nx2NPred );
